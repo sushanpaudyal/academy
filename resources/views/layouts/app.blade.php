fa2aebbbc74bdfd2697b80be802cccc2fe32a6dc
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-07-07 16:30:19 +0545

    Error solving

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="keywords" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>TheSaaS - Responsive Bootstrap SaaS, Software & WebApp Template</title>
 
@@ -17,12 +18,7 @@
     <link rel="apple-touch-icon" href="{{asset('assets/img/apple-touch-icon.png')}}">
     <link rel="icon" href="{{asset('assets/img/favicon.png')}}">
 
-    <!--  Open Graph Tags -->
-    <meta property="og:title" content="TheSaaS">
-    <meta property="og:description" content="A responsive, professional, and multipurpose SaaS, Software, Startup and WebApp landing template powered by Bootstrap 4.">
-    <meta property="og:image" content="http://thetheme.io/thesaas/assets/img/og-img.jpg">
-    <meta property="og:url" content="http://thetheme.io/thesaas/">
-    <meta name="twitter:card" content="summary_large_image">
+
 </head>
 
 <body>
@@ -67,60 +63,24 @@
      <!-- Footer -->
      <footer class="site-footer">
          <div class="container">
-             <div class="row gap-y align-items-center">
-                 <div class="col-12 col-lg-3">
-                     <p class="text-center text-lg-left">
-                         <a href="index.html"><img src="{{asset('assets/img/logo.png')}}" alt="logo"></a>
-                     </p>
-                 </div>
-
+             <div class="row gap-y justify-content-center">
                  <div class="col-12 col-lg-6">
                      <ul class="nav nav-primary nav-hero">
-                         <li class="nav-item">
-                             <a class="nav-link" href="index.html">Home</a>
-                         </li>
-                         <li class="nav-item">
-                             <a class="nav-link" href="blog.html">Blog</a>
-                         </li>
-                         <li class="nav-item">
-                             <a class="nav-link" href="block-feature.html">Features</a>
-                         </li>
                          <li class="nav-item hidden-sm-down">
-                             <a class="nav-link" href="page-pricing.html">Pricing</a>
-                         </li>
-                         <li class="nav-item hidden-sm-down">
-                             <a class="nav-link" href="page-contact.html">Contact</a>
+                             <a class="nav-link" href="/">Bahdcasts</a>
                          </li>
                      </ul>
-                 </div>
-
-                 <div class="col-12 col-lg-3">
-                     <div class="social text-center text-lg-right">
-                         <a class="social-facebook" href="https://www.facebook.com/thethemeio"><i class="fa fa-facebook"></i></a>
-                         <a class="social-twitter" href="https://twitter.com/thethemeio"><i class="fa fa-twitter"></i></a>
-                         <a class="social-instagram" href="https://www.instagram.com/thethemeio/"><i class="fa fa-instagram"></i></a>
-                         <a class="social-dribbble" href="https://dribbble.com/thethemeio"><i class="fa fa-dribbble"></i></a>
-                     </div>
                  </div>
              </div>
          </div>
      </footer>
      <!-- END Footer -->
+ </div>
 
-
-     <a class="scroll-top" href="#"><i class="fa fa-angle-up"></i></a>
-
-
-     <!-- Scripts -->
-     <script src="{{asset('assets/js/core.min.js')}}"></script>
-     <script src="{{asset('assets/js/thesaas.min.js')}}"></script>
-     <script src="{{asset('assets/js/script.js')}}"></script>
-     <script src="{{asset('js/app.js')}}"></script>
-
-
-
-
-
-
-</div>
+ <!-- Scripts -->
+ <script src="{{ asset('assets/js/core.min.js') }}"></script>
+ <script src="{{ asset('assets/js/thesaas.min.js') }}"></script>
+ <script src="{{ asset('assets/js/script.js') }}"></script>
+ <script src="{{ asset('js/app.js') }}"></script>
 </body>
+</html>
